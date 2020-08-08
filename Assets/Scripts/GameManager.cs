72a6f447aab42f5947787d4714beca944cc134f1
Vladimir Montchik <vam4@illinois.edu>
2020-08-07 20:01:05 -0500

    initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private Cube ally;
+    private Cube enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // each turn, each block gets to attack
+    }
+}
