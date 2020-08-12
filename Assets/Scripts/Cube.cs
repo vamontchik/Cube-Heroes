9e68fb656679f00e6edf0cb8ab474191754c1347
Vladimir Montchik <vam4@illinois.edu>
2020-08-11 20:28:29 -0500

    properties, private methods, and a speed mechanic

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -2,71 +2,41 @@
 
 public class Cube
 {
-    private int health;
-    private int maxHealth;
-    private int attackMin;
-    private int attackMax;
-    private int defense;
-    // private int speed;
-    private int critRate;
-    private double critDamage;
     // private int resistance;
     // private int accuracy;
 
-    // needs to be static, or all cubes
-    // have duplicate moves... all get same initial seed?
+    // needs to be static, or all cubes have duplicate moves... all get same initial seed?
     private static Random rand = new Random();
 
-    private string name;
+    public int Health { get; set; }
+    public int MaxHealth { get; set; }
+    public int AttackMin { get; set; }
+    public int AttackMax { get; set; }
+    public int Defense { get; set; }
+    public int TurnValue { get; set; }
+    public int MaxTurnValue { get; set; }
+    public int Speed { get; set; }
+    public int CritRate { get; set; }
+    public double CritDamage { get; set; }
+    public string Name { get; set; }
 
-    // TODO: use builder pattern here...
-    public Cube(int health, int maxHealth, int attackMin, int attackMax, int defense, int critRate, double critDamage, string name)
-    {
-        this.health = health;
-        this.maxHealth = maxHealth;
-        this.attackMin = attackMin;
-        this.attackMax = attackMax;
-        this.defense = defense;
-        // this.speed = 0;
-        this.critRate = critRate;
-        this.critDamage = critDamage;
-        // this.resistance = 0;
-        // this.accuracy = 0;
-
-        this.name = name;
-    }
-
-    public int GetHealth()
-    {
-        return this.health;
-    }
-
-    public int GetMaxHealth()
-    {
-        return this.maxHealth;
-    }
-
-    //public string GetName()
-    //{
-    //    return this.name;
-    //}
 
     public AttackResult Attack(Cube enemy)
     {
         AttackResult resultObj = new AttackResult();
 
-        resultObj.isCrit = rand.Next(/*inclusive*/ 0, /*exclusive*/ 101) < critRate;
+        resultObj.isCrit = rand.Next(/*inclusive*/ 0, /*exclusive*/ 101) < CritRate;
         
-        int tempAttack = rand.Next(/*inclusive*/ this.attackMin, /*exclusive*/ this.attackMax);
-        int actualAttack = tempAttack + (int)Math.Floor(resultObj.isCrit ? tempAttack * this.critDamage : 0);
+        int tempAttack = rand.Next(/*inclusive*/ AttackMin, /*exclusive*/ AttackMax);
+        int actualAttack = tempAttack + (int)Math.Floor(resultObj.isCrit ? tempAttack * CritDamage : 0);
         
-        resultObj.enemyDefense = enemy.defense;
+        resultObj.enemyDefense = enemy.Defense;
         
-        resultObj.damageApplied = Math.Max(0, actualAttack - enemy.defense);
+        resultObj.damageApplied = Math.Max(0, actualAttack - enemy.Defense);
         
-        enemy.health -= resultObj.damageApplied;
+        enemy.Health -= resultObj.damageApplied;
         
-        resultObj.isEnemyDead = enemy.health <= 0;
+        resultObj.isEnemyDead = enemy.Health <= 0;
 
         return resultObj;
     }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,13 +9,10 @@ public class GameManager : MonoBehaviour
 {
     // OO objects
     private Cube ally;
+    private List<Cube> allies;
     private Cube enemy;
-    private readonly int PLAYER_AMOUNT = 2;
-
-    private Turn turn;
-    private int currentTurn;
-    private readonly int ALLY_TURN = 0;
-    //private readonly int ENEMY_TURN = 1;
+    private List<Cube> enemies;
+    private List<Cube> allCubes;
 
     private List<TextMeshPro> listOfDamageNumbers;
     private readonly float DELETE_THRESHOLD = 0.01f;
@@ -26,94 +23,180 @@ public class GameManager : MonoBehaviour
     public Transform allyTransform;
     public Transform enemyTransform;
 
-    public Slider allySlider;
-    public Slider enemySlider;
+    public Slider allyHealthSlider;
+    public Slider enemyHealthSlider;
+
+    public Slider enemySpeedSlider;
 
     public TextMeshPro damageNumbers;
 
     // Start is called before the first frame update
-    void Start() 
+    void Start()
     {
-        ally = new Cube(100, 100, 6, 16, 5, 25, 1.5, "ally");
-        enemy = new Cube(100, 100, 6, 16, 5, 25, 1.5, "enemy");
-        turn = new Turn(PLAYER_AMOUNT);
-        currentTurn = ALLY_TURN;
+        ally = new Cube
+        {
+            Health = 100,
+            MaxHealth = 100,
+            AttackMin = 6,
+            AttackMax = 16,
+            Defense = 5,
+            TurnValue = 0,
+            MaxTurnValue = 100,
+            Speed = 100,
+            CritRate = 25,
+            CritDamage = 1.5,
+            Name = "ally"
+        };
+
+
+        enemy = new Cube
+        {
+            Health = 100,
+            MaxHealth = 100,
+            AttackMin = 6,
+            AttackMax = 16,
+            Defense = 5,
+            TurnValue = 0,
+            MaxTurnValue = 100,
+            Speed = 50,
+            CritRate = 25,
+            CritDamage = 1.5,
+            Name = "enemy"
+        };
+
+        allies = new List<Cube>
+        {
+            ally
+        };
+
+        enemies = new List<Cube>
+        {
+            enemy
+        };
+
+        allCubes = new List<Cube> 
+        { 
+            ally, enemy 
+        };
 
         listOfDamageNumbers = new List<TextMeshPro>();
     }
 
-    // Update is called once per frame
-    void Update() {
+    private List<Cube> UpdateSpeeds()
+    {
+        List<Cube> cubesToMove = new List<Cube>();
+
+        allCubes.ForEach(cube => {
+            cube.TurnValue += cube.Speed;
+            if (cube.TurnValue >= cube.MaxTurnValue)
+            {
+                cube.TurnValue -= cube.MaxTurnValue;
+                cubesToMove.Add(cube);
+            }
+        });
+
+        return cubesToMove;
+        
+    }
+
+    private void CleanUpDamageNumbers()
+    {
         List<TextMeshPro> toDelete = listOfDamageNumbers.Where(dmgNum => dmgNum.color.a <= DELETE_THRESHOLD).ToList();
         listOfDamageNumbers = listOfDamageNumbers.Where(dmgNum => dmgNum.color.a > DELETE_THRESHOLD).ToList();
         toDelete.ForEach(x => Destroy(x));
-        
+    }
+
+    private void UpdatePositionsForDamageNumbers()
+    {
         listOfDamageNumbers.ForEach(dmgNum => {
             dmgNum.color = new Color(dmgNum.color.r, dmgNum.color.g, dmgNum.color.b, dmgNum.color.a - (0.5f * Time.deltaTime));
             dmgNum.transform.position += new Vector3(0, 2f) * Time.deltaTime;
         });
-
-        sinceLastUpdate += Time.deltaTime;
-        if (sinceLastUpdate < 1.0) return;
-        sinceLastUpdate = 0f;
-
-        AttackResult result = new AttackResult();
-        currentTurn = turn.NextTurn();
-
-        //string toPrint;
-        TextMeshPro created;
-        if (currentTurn == ALLY_TURN)
+    }
+    private Cube PickRandomEnemy(Cube cube)
+    {
+        if (cube == ally)
         {
-            result = ally.Attack(enemy);
-            enemySlider.value = 1.0f * enemy.GetHealth() / enemy.GetMaxHealth();
+            return enemy;
+        } 
+        else
+        {
+            return ally;
+        }
+    }
 
-            created = Instantiate(damageNumbers, enemyTransform.position + 2 * Vector3.up, Quaternion.Euler(0, -90, 0));
-            created.SetText(result.damageApplied.ToString());
-            listOfDamageNumbers.Add(created);
+    private void ModifyWithCritAsNecessary(TextMeshPro created, AttackResult result)
+    {
+        if (result.isCrit)
+        {
+            created.color = Color.red;
+        }
+    }
 
-            //toPrint = String.Format(
-            //    "{0} attacks {1} for {2} damage! {0} Health: {3}, {1} Health: {4}",
-            //    ally.GetName(),
-            //    enemy.GetName(),
-            //    result.damageApplied,
-            //    ally.GetHealth(),
-            //    enemy.GetHealth()
-            //);
-
+    private TextMeshPro CreateDamagePopup(Cube attackedCube, AttackResult result)
+    {
+        Transform attackedCubeTransform;
+        if (attackedCube == ally)
+        {
+            attackedCubeTransform = allyTransform;
         }
         else
         {
-            result = enemy.Attack(ally);
-            allySlider.value = 1.0f * ally.GetHealth() / ally.GetMaxHealth();
-
-            created = Instantiate(damageNumbers, allyTransform.position + 2 * Vector3.up, Quaternion.Euler(0, -90, 0));
-            created.SetText(result.damageApplied.ToString());
-            listOfDamageNumbers.Add(created);
-
-            //toPrint = String.Format(
-            //    "{0} attacks {1} for {2} damage! {1} Health: {3}, {0} Health: {4}",
-            //    enemy.GetName(),
-            //    ally.GetName(),
-            //    result.damageApplied,
-            //    ally.GetHealth(),
-            //    enemy.GetHealth()
-            //);
+            attackedCubeTransform = enemyTransform;
         }
 
-        if (result.isCrit)
-        {
-            //toPrint = "Critical Hit! " + toPrint;
-            created.color = Color.red;
-        }
-        //Debug.Log(toPrint);
+        TextMeshPro created = Instantiate(damageNumbers, attackedCubeTransform.position + 2 * Vector3.up, Quaternion.Euler(0, -90, 0));
+        created.SetText(result.damageApplied.ToString());
+        listOfDamageNumbers.Add(created);
 
-        if (result.isEnemyDead)
+        return created;
+    }
+
+    private void UpdateSlider(Slider ofAttackedCube, Cube attackedCube)
+    {
+        ofAttackedCube.value = 1.0f * attackedCube.Health / attackedCube.MaxHealth;
+    }
+
+    private Slider GetCorrespondingSlider(Cube attackedCube)
+    {
+        if (attackedCube == ally)
         {
+            return allyHealthSlider;
+        }
+        else
+        {
+            return enemyHealthSlider;
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        CleanUpDamageNumbers();
+        UpdatePositionsForDamageNumbers();
+
+        // barrier for updating actions every second
+        sinceLastUpdate += Time.deltaTime;
+        if (sinceLastUpdate < 1.0f) return;
+        sinceLastUpdate = 0f;
+
+        List<Cube> cubesToMove = UpdateSpeeds();
+        cubesToMove.ForEach(cube =>
+        {
+            Cube randEnemy = PickRandomEnemy(cube);
+            AttackResult result = cube.Attack(randEnemy);
+            Slider enemySlider = GetCorrespondingSlider(randEnemy);
+            UpdateSlider(enemySlider, randEnemy);
+            TextMeshPro created = CreateDamagePopup(randEnemy, result);
+            ModifyWithCritAsNecessary(created, result);
+
+            if (result.isEnemyDead)
+            {
 #if UNITY_EDITOR
-            UnityEditor.EditorApplication.isPlaying = false;
+                UnityEditor.EditorApplication.isPlaying = false;
 #else
-            Application.Quit();
+                Application.Quit();
 #endif
-        }
+            }
+        });
     }
 }
